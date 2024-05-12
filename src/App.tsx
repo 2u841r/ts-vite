5f2ac246fb27d0cc15624c1527d5818c4dd9a227
Zubair <77893877+2u841r@users.noreply.github.com>
2024-05-12 09:45:36 +0600

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import Layout from "./components/Layout";
 import Testts from "./components/Testts";
